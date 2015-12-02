527ca5b405aa08d49e3583d3516ec919dbeef2fd
Simon Brydon <simonbrydon@gmail.com>
2015-12-02 02:29:17 +0100

    Add Random & String extensions

---
+++ Norbert/Norbert.Modules.Common/Helpers/RandomExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Norbert.Modules.Common.Helpers
+{
+    public static class RandomExtensions
+    {
+        public static double NextDouble(this Random random, double minValue, double maxValue,
+            int digits = 4)
+        {
+            var value = random.NextDouble() * (maxValue - minValue) + minValue;
+            return Math.Round(value, digits);
+        }
+    }
+}
---
+++ Norbert/Norbert.Modules.Common/Helpers/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace Norbert.Modules.Common.Helpers
+{
+    public static class StringExtensions
+    {
+        public static string Truncate(this string value, int length, string pad = "..")
+        {
+            if (value.Length <= length)
+                return value;
+
+            if (length <= pad.Length)
+                return pad.Substring(0, length);
+
+            return value.Substring(0, length - pad.Length).TrimEnd() + pad;
+        }
+    }
+}
